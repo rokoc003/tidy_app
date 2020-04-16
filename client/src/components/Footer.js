f244b210ae2dcd0123e6c5cf0e6a1484e254c76f
rokoc003 <roxane.kocem@yahoo.de>
2020-04-16 21:22:26 +0200

    Optimize and style footer component

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -4,11 +4,15 @@ import PropTypes from 'prop-types';
 
 const FooterContainer = styled.div`
   display: flex;
-  justify-content: space-around;
+  justify-content: center;
   width: 100%;
+  height: 50px;
 `;
 
 const FooterIcon = styled.button`
+  display: flex;
+  justify-content: center;
+  min-width: 100px;
   cursor: pointer;
   border: none;
   &:focus {
@@ -16,11 +20,17 @@ const FooterIcon = styled.button`
   }
 `;
 
-function Footer({ links }) {
+function Footer({ links, isActive, onItemClick }) {
   return (
     <FooterContainer>
       {links.map((link) => (
-        <FooterIcon key={link.Label}>{link.icon}</FooterIcon>
+        <FooterIcon
+          key={link.label}
+          active={isActive === link.label}
+          onClick={() => onItemClick(link.label)}
+        >
+          <link.Icon active={isActive === link.label} />
+        </FooterIcon>
       ))}
     </FooterContainer>
   );
@@ -28,6 +38,8 @@ function Footer({ links }) {
 
 Footer.propTypes = {
   links: PropTypes.array,
+  isActive: PropTypes.string,
+  onItemClick: PropTypes.func,
 };
 
 export default Footer;
--- client/src/stories/Footer.stories.js
+++ client/src/stories/Footer.stories.js
@@ -3,20 +3,24 @@ import Footer from '../components/Footer';
 import Home from '../assets/icons/Home';
 import List from '../assets/icons/List';
 import Plus from '../assets/icons/Plus';
+import { action } from '@storybook/addon-actions';
+import { withKnobs, text } from '@storybook/addon-knobs';
 
 export default {
   title: 'Footer',
-  component: 'Footer',
+  decorators: [withKnobs],
 };
 
 export const Default = () => {
   return (
     <Footer
       links={[
-        { label: 'Home', icon: <Home /> },
-        { label: 'List', icon: <List /> },
-        { label: 'Plus', icon: <Plus /> },
+        { label: 'Home', Icon: Home },
+        { label: 'List', Icon: List },
+        { label: 'Plus', Icon: Plus },
       ]}
+      isActive={text('value', 'Home')}
+      onItemClick={action('item-click')}
     />
   );
 };
