98c3d4dc0f31b3fafd3fbf70b84bb34273ddc6e9
rokoc003 <roxane.kocem@yahoo.de>
2020-04-22 10:48:03 +0200

    Create new component for Signup page

---
+++ client/src/components/PageCaption.js
@@ -0,0 +1,10 @@
+import styled from '@emotion/styled';
+import colors from '../utils/colors';
+
+const PageCaption = styled.span`
+  color: ${colors.captionFont};
+  margin: 10px 0px;
+  font: 500 1.3rem 'Ubuntu';
+`;
+
+export default PageCaption;
