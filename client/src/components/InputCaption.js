833dfad44b30ea7d5ee9036a7dd512b684bd6a81
rokoc003 <roxane.kocem@yahoo.de>
2020-04-22 19:10:28 +0200

    Create component for Input captions

---
+++ client/src/components/InputCaption.js
@@ -0,0 +1,11 @@
+import styled from '@emotion/styled';
+import colors from '../utils/colors';
+
+const InputCaption = styled.span`
+  color: ${colors.captionFont};
+  margin: 10px 0px;
+  font-size: 1.2rem;
+  font-weight: 400;
+`;
+
+export default InputCaption;
