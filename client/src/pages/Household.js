997887f9f5265b53528a78c12d78c694664ddd80
rokoc003 <roxane.kocem@yahoo.de>
2020-05-04 13:47:53 +0200

    Add functionality to household page

--- client/src/pages/Household.js
+++ client/src/pages/Household.js
@@ -1,4 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import { createHousehold } from '../api/household';
+import DeleteIcon from '../assets/icons/Icon-trash.svg';
 import InputCircle from '../components/InputCircle';
 import InputLine from '../components/InputLine';
 import InputContainer from '../components/InputContainer';
@@ -10,32 +13,136 @@ import ButtonContainer from '../components/ButtonContainer';
 import Form from '../components/Form';
 import Wrapper from '../components/Wrapper';
 import PageContainer from '../components/PageContainer';
+import styled from '@emotion/styled';
+
+const DeleteButton = styled.img`
+  display: flex;
+  justify-content: center;
+  width: 20px;
+  cursor: pointer;
+  border: none;
+  &:focus {
+    outline: none;
+  }
+`;
 
 function Household() {
+  const [name, setName] = useState('');
+  const [rooms, setRooms] = useState([]);
+  const [members, setMembers] = useState([]);
+  const [member, setMember] = useState('');
+  const [room, setRoom] = useState('');
+
+  function addToRoomList(newRoom) {
+    const newItem = [...rooms, newRoom];
+    setRooms(newItem);
+  }
+
+  function addToMemberList(newMember) {
+    const newItem = [...members, newMember];
+    setMembers(newItem);
+  }
+
+  const handleSubmitRoom = (event) => {
+    event.preventDefault();
+    if (!room) return;
+    addToRoomList(room);
+    setRoom('');
+  };
+
+  const handleSubmitMember = (event) => {
+    event.preventDefault();
+    if (!member) return;
+    addToMemberList(member);
+    setMember('');
+  };
+
+  const handleDeleteRoom = (id) => {
+    const newRooms = [...rooms];
+    newRooms.splice(id, 1);
+    setRooms(newRooms);
+  };
+
+  const handleDeleteMember = (id) => {
+    const newMembers = [...members];
+    newMembers.splice(id, 1);
+    setMembers(newMembers);
+  };
+
   return (
     <PageContainer>
       <Wrapper>
         <Header />
         <PageCaption>Your household</PageCaption>
       </Wrapper>
-      <Form>
-        <InputContainer>
-          <InputCircle type="text" placeholder="Enter householdname" />
-        </InputContainer>
-        <InputContainer>
-          <InputCaption>Create household</InputCaption>
-          <InputLine type="text" placeholder="Add room" />
-        </InputContainer>
-        <InputContainer>
-          <InputCaption>Household members</InputCaption>
-          <InputLine type="text" placeholder="Add name" />
-        </InputContainer>
-      </Form>
+      <InputContainer>
+        <InputCircle
+          value={name}
+          onChange={(event) => setName(event.target.value)}
+          placeholder="Enter householdname"
+        />
+      </InputContainer>
+      <InputContainer>
+        <InputCaption>Create household</InputCaption>
+        <Form onSubmit={handleSubmitRoom}>
+          <InputLine
+            value={room}
+            onChange={(event) => setRoom(event.target.value)}
+            placeholder="Add room"
+          />
+        </Form>
+        {rooms.map((room) => (
+          <>
+            <span key={room.id} room={room}>
+              {room}
+            </span>
+            <DeleteButton
+              src={DeleteIcon}
+              onClick={() => {
+                handleDeleteRoom();
+              }}
+            />
+          </>
+        ))}
+      </InputContainer>
+      <InputContainer>
+        <InputCaption>Household members</InputCaption>
+        <Form onSubmit={handleSubmitMember}>
+          <InputLine
+            value={member}
+            onChange={(event) => setMember(event.target.value)}
+            placeholder="Add name"
+          />
+        </Form>
+        {members.map((member) => (
+          <>
+            <span key={member.id} member={member}>
+              {member}
+            </span>
+            <DeleteButton
+              src={DeleteIcon}
+              onClick={() => {
+                handleDeleteMember();
+              }}
+            />
+          </>
+        ))}
+      </InputContainer>
       <ButtonContainer>
-        <Button>Submit</Button>
+        <Button
+          onClick={() => {
+            createHousehold({ name, rooms, members });
+          }}
+        >
+          Submit
+        </Button>
       </ButtonContainer>
     </PageContainer>
   );
 }
 
+Household.propTypes = {
+  onChange: PropTypes.func,
+  value: PropTypes.string,
+};
 export default Household;
