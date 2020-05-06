8f6bcbe448d79bb4c6a4b3b007ffc48484cc072f
rokoc003 <roxane.kocem@yahoo.de>
2020-05-06 17:04:51 +0200

    Outsource delete button

---
+++ client/src/components/DeleteButton.js
@@ -0,0 +1,14 @@
+import styled from '@emotion/styled';
+
+const DeleteButton = styled.img`
+  display: flex;
+  justify-content: center;
+  width: 20px;
+  cursor: pointer;
+  border: none;
+  &:focus {
+    outline: none;
+  }
+`;
+
+export default DeleteButton;
