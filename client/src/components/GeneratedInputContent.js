6063f3a03caf6be787eb28fe84635e52101e2670
rokoc003 <roxane.kocem@yahoo.de>
2020-05-07 12:30:41 +0200

    Change naming

---
+++ client/src/components/GeneratedInputContent.js
@@ -0,0 +1,9 @@
+import styled from '@emotion/styled';
+
+const GeneratedInputContent = styled.span`
+  justify-content: flex-start;
+  font-size: 1.2rem;
+  margin: 0px 10px;
+`;
+
+export default GeneratedInputContent;
--- client/src/components/ListItem.js
+++
@@ -1,9 +0,0 @@
-import styled from '@emotion/styled';
-
-const ListItem = styled.span`
-  display: flex;
-  font-size: 1.2rem;
-  margin-right: 5px;
-`;
-
-export default ListItem;
