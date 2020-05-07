37b27864dc575b406c9d65a57a6a2411b9641051
rokoc003 <roxane.kocem@yahoo.de>
2020-05-07 12:59:03 +0200

    Modify input components

--- client/src/components/InputLine.js
+++ client/src/components/InputLine.js
@@ -5,7 +5,8 @@ import colors from '../utils/colors';
 const InputLine = styled(Input)`
   padding: 2px 8px;
   height: 25px;
-  border-bottom: 4px solid ${colors.InputLine};
+  border-bottom: 2px solid ${colors.InputLine};
+  margin-bottom: 15px;
 `;
 
 export default InputLine;
